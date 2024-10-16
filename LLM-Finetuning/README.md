7f33c631bcad4e9a7c4a6a5558ea42a5c151788c
Péter Molnár <pmolnar@gsu.edu>
2024-10-15 20:14:48 -0400

    initial commit

---
+++ LLM-Finetuning/README.md
@@ -0,0 +1,5 @@
+# LLM Fine-tuning
+
+This activity demonstrates fine-tuning a Large Language Model.
+We use a fairly small model (llama2) for this exercise, though the process remains the same for larger models.
+
---
+++ LLM-Finetuning/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script is used to build the project and run the tests.
+
+# determine whether podman or docker should be used
+if [ -x "$(command -v podman)" ]; then
+    DOCKER=podman
+    DEVICES="--device nvidia.com/gpu=0 --device nvidia.com/gpu=1 --device nvidia.com/gpu=2 --device nvidia.com/gpu=3 --security-opt=label=disable"
+elif [ -x "$(command -v docker)" ]; then
+    DOCKER=docker
+    DEVICES="--device /dev/dri:/dev/dri"
+else
+    echo "Neither podman nor docker is installed. Please install one of them."
+    exit 1
+fi
+
+CONTAINER_NAME="${USER}/nvidia-ml-pytorch"
+
+HUGGINGFACE_TOKEN=$(cat $HOME/.secretes/huggingface_token.txt)
+
+$DOCKER build -t $CONTAINER_NAME \
+    --build-arg USER="$USER" \
+    --build-arg UID=$(id -u) \
+    --build-arg GID=$(id -g) \
+    --build-arg UNAME=$(uname -m) \
+    --build-arg NVIDIA_VISIBLE_DEVICES=all \
+    -f Dockerfile .
+
+$DOCKER run --rm -it \
+    -e HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN \
+    --name $CONTAINER_NAME \
+    --user $(id -u):$(id -g) \
+    --workdir /app $*
+
+
+
